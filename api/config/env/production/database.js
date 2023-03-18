8b4b3fd746337b4da6288016e085c1c7658aa3bb
matti909 <matias.saantiago@gmail.com>
2023-03-17 23:03:33 -0300

    1

--- api/config/env/production/database.js
+++ api/config/env/production/database.js
@@ -6,11 +6,27 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
+      host: dbConfig.host,
+      port: dbConfig.port,
+      database: dbConfig.database,
+      user: dbConfig.user,
+      password: dbConfig.password,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+  },
+});
+
+/* module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
       host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'password'),
+      user: env('DATABASE_USERNAME', 'matti'),
+      password: env('DATABASE_PASSWORD', '34898525'),
       schema: env('DATABASE_SCHEMA', 'public'), // Not required
       ssl: {
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
@@ -18,4 +34,4 @@ module.exports = ({ env }) => ({
     },
     debug: false,
   },
-});
+}); */
